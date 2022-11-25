a0e39f7b7072ae3e7782e04ef2ee0a94d41a5e19
klaydm <klaydm@gmail.com>
2022-11-24 23:47:38 -0300

    Consumindo API de formatação de data, utilizando o Local Storage

--- TODO/main.js
+++ TODO/main.js
@@ -1,23 +1,7 @@
-import BotaoConcluido from "./components/tarefaConcluida.js";
-import BotaoExcluir from "./components/excluirTarefa.js";
-
-  const criarTarefa = (evento) => {
-  
-  evento.preventDefault ();
-
-  const lista = document.querySelector("[data-list]")
-  const input = document.querySelector("[data-form-input]");
-  const valor = input.value;
-  const tarefa = document.createElement("li");
-  tarefa.classList.add("task");
-  const conteudo = `<p class="content">${valor}</p>`;
-  tarefa.innerHTML = conteudo;
-  tarefa.appendChild(BotaoConcluido());
-  tarefa.appendChild(BotaoExcluir());
-  lista.appendChild(tarefa);
-  input.value = " ";
-
-}
+import { handleNovoItem } from "./components/criaTarefa.js";
+import { carregaTarefa } from "./components/carregaTarefa.js";
 
 const novaTarefa = document.querySelector("[data-form-buttom]");
-  novaTarefa.addEventListener("click", criarTarefa);
+novaTarefa.addEventListener("click", handleNovoItem);
+
+carregaTarefa();
